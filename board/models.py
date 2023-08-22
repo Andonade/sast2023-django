1067ac9efa0a8b002c28255b55d537fed623e1fb
Andonade <m18965691135@163.com>
2023-08-22 21:50:12 +0800

    feat: complete model

--- board/models.py
+++ board/models.py
@@ -41,7 +41,15 @@ class Board(models.Model):
     # Meta data
     # Create index on board_name
     # Create unique_together on user and board_name
-    
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board_state = models.CharField(max_length=50)
+    board_name = models.CharField(max_length=50)
+    create_time = models.FloatField(default=utils_time.get_timestamp)
+
+    class META:
+        indexes = [models.Index(fields=["board_name"], name="board_name_idx")]
+        unique_together = ["user", "board_name"]
     # TODO End: [Student] Finish the model of Board
 
 
